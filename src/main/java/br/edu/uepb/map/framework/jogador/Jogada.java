2aae38cf7bf7931800ac854d05dc0635d50f9a9f
Joaquim Laureano <joaquimlaureanog7@gmail.com>
2026-08-12 14:44:51 -0300

    feat: cria Jogador(Classe abstrata) e Jogada(Interface marcadora)

---
+++ src/main/java/br/edu/uepb/map/framework/jogador/Jogada.java
@@ -0,0 +1,8 @@
+package br.edu.uepb.map.framework.jogador;
+
+/**
+ * Marca um tipo de jogada. Cada jogo concreto define suas próprias
+ * jogadas possíveis.
+ */
+public interface Jogada {
+}
--- src/main/java/br/edu/uepb/map/framework/jogador/Jogador.java
+++ src/main/java/br/edu/uepb/map/framework/jogador/Jogador.java
@@ -1,4 +1,39 @@
 package br.edu.uepb.map.framework.jogador;
 
-public class Jogador {
+import br.edu.uepb.map.framework.cartas.Carta;
+
+public abstract class Jogador {
+
+    private final String nome;
+    private final MaoDeCartas<Carta> maoDeCartas;
+    private boolean ativo;
+
+    protected Jogador(String nome, MaoDeCartas<Carta> maoDeCartas) {
+        this.nome = nome;
+        this.maoDeCartas = maoDeCartas;
+        this.ativo = true;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public MaoDeCartas<Carta> getMaoDeCartas() {
+        return maoDeCartas;
+    }
+
+    //Indica se o jogador ainda participa ativamente da rodada atual
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    protected void desativar() {
+        this.ativo = false;
+    }
+
+    public void receberCarta(Carta carta) {
+        maoDeCartas.receberCarta(carta);
+    }
+
+    public abstract Jogada decidirJogada();
 }
